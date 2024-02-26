e65b9d2aa7059de3b45154a545162a4a19090b91
Shouko <shouko4632@gmail.com>
2024-02-26 04:55:32 +0300

    Добавляет footer и header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,21 @@
 /* в этот файл добавляет скрипты*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navList = document.querySelector('.site-nav__list');
+  const toggleButton = document.querySelector('.js-toggle-button');
+  const svgIcon = toggleButton.querySelector('.menu-list__icon use');
+
+  navList.classList.add('site-nav__list--js-loaded');
+  navList.classList.add('site-nav__list--closed');
+  svgIcon.setAttribute('href', 'images/icons/stack.svg#menu-burger');
+
+  toggleButton.addEventListener('click', () => {
+    if (svgIcon.getAttribute('href').endsWith('menu-burger')) {
+      svgIcon.setAttribute('href', 'images/icons/stack.svg#menu-cross');
+      navList.classList.remove('site-nav__list--closed');
+    } else {
+      svgIcon.setAttribute('href', 'images/icons/stack.svg#menu-burger');
+      navList.classList.add('site-nav__list--closed');
+    }
+  });
+});
